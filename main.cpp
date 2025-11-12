2d3c8674c725114aa06002842ffb3d1b9c8d57b6
Ali-Issa-8792 <ali.issa@student.giu-uni.de>
2025-11-12 15:07:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,8 +9,17 @@ using namespace std;
 
 
 // ============== MAIN FUNCTION ==============
-int main() {
-    
-    
+int main(){
+
+    Enclosure enclosure(5, 0);
+    Animal* Lion = new Mammal("Lion", 2, true, "Orange");
+    Animal* Parrot = new Bird("Parrot", 2, false, 10.1);
+    Animal* Snake =  new Reptile("Snake", 3, true ,true);
+    Visitor visitor("Sarah Ali", 3);
+
+
+    displayAnimals();
+    vistor.displayInfo();
     return 0;
+
 }
