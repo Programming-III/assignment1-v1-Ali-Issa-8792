58c678e8c09a5f5a2db0dfe5e3e3e38fa12681f5
Ali-Issa-8792 <ali.issa@student.giu-uni.de>
2025-11-12 15:10:23 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -4,7 +4,40 @@
 #include <string>
 using namespace std;
 
-//define animal class here 
+class Animal{
+private:
+    string name;
+    int age;
+    bool isHungry;
+
+public:
+    Animal(string n, int a, bool hungry) : name(n), age(a), isHungry(hungry) {}
+    ~Animal() {}
+    void display();
+    void feed();
+
+    string getName()
+    {
+        return name;
+    }
+    int getAge()
+    {
+        return age;
+    }
+    bool getIsHungry()
+    {
+        return isHungry;
+    }
+
+    string Hunger()
+    {
+        if(Animal.isHungry == true)
+            return "is Hungry";
+        else
+            return "is Not Hungry";
+    }
+
+}
 
 
 
