d84050b479832d74f1eb20e6281f1cf5b0d9f06c
Ali-Issa-8792 <ali.issa@student.giu-uni.de>
2025-11-12 15:12:48 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -3,7 +3,31 @@
 
 #include "Animal.h"
 
-//define enclosure class here 
+class Enclosure{
+    private:
+        Animal* animals[];
+        int capacity;
+        int currentCount;
+    public:
+        Enclosure(int cap, int curr){
+            Animal* animals[];
+            capacity =  cap;
+            currentCount =  curr;
+        }
+        ~Enclosure() {}
+
+        void addAnimal(Animal* a) {
+            animals.add(a);
+            currentCount++;
+        }
+        void displayAnimals() {
+            for(int i = 0; i < currentCount ;  i++)
+            {
+                cout << animals[i].getName(); << " Age: " << animals[i].getAge(); << animals[i].Hunger(); << endl;
+            }
+        }
+
+};
 
 
 
