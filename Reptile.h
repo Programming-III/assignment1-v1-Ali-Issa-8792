20ac269c8f63170de32ec10da0f56eb1a301dfdb
Ali-Issa-8792 <ali.issa@student.giu-uni.de>
2025-11-12 15:08:15 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -5,7 +5,15 @@
 
 //define reptile class here
 
+class Reptile : Animal {
+    private:
+        bool isVenomous;
+    public:
+        Reptile(string n, int a, bool hungry, bool venom) : Animal(name, age, isHungry), isVenomous(venom) {}
+        ~Reptile() {}
+        bool getVenom();
 
+};
 
 
 
